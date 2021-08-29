d663d346f91674157e75696a3e01a0a7fc0622c2
Kong Kunru <apexbugfinder@gmail.com>
2021-08-28 22:18:48 -0700

    experience page complete

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as fromUser from '../../user/searchbar-results/state';
+import * as searchBarActions from '../../user/searchbar/state/searchbar.actions';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() {   }
+  resultsTotal$: Observable<number>;
+  constructor(private searchBarStore: Store<fromUser.SearchBarResultsDataState>) {
+    this.resultsTotal$ = this.searchBarStore.pipe(select(fromUser.selectUsersTotal));
+   }
 
   ngOnInit(): void {
-  
+
   }
 
 }
